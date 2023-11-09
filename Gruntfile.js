4942bbde55638f7641ef0481603fcd4037f990eb
IMeshev <igor.meshev@3shape.com>
2023-11-09 13:20:53 +0100

    Test

--- Gruntfile.js
+++ Gruntfile.js
@@ -293,7 +293,7 @@ module.exports = function(grunt) {
 
             options : {
 
-                noCache : true,  // Don't cache to sassc files.
+                noCache : false,  // Don't cache to sassc files.
                 precision : 14, // How many digits of precision to use when outputting decimal numbers.
                 //sourcemap : 'none', // Generate CSS source maps?
 
